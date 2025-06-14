ba1df8f9c6bbdc08ed015a6b6139b5c55b71112f
Rahul Adhikari <raholea@gmail.com>
2025-06-14 16:31:53 +0545

    last changes

--- basic1.py
+++ basic1.py
@@ -1,5 +1,28 @@
-print("This is the development branch!")
-print("Testing changes before merging.")
-print("Name: Rahul Adhikari")
-print("College: Amrit Science Campus")
-print("Python is powerful")
+\documentclass{article}
+\usepackage[utf8]{inputenc}
+
+\title{SICNAP 2025 Internship Report}
+\author{Rahul Adhikari}
+\date{\today}
+
+\begin{document}
+
+\maketitle
+
+\section{Introduction}
+This report documents the internship tasks completed during SICNAP 2025.
+
+\section{Part A: Bash & Linux}
+Worked with shell scripting, file permissions, and command-line utilities.
+
+\section{Part B: Python Programming}
+Practiced logic, loops, file I/O, and functions.
+
+\section{Part C: Git & GitHub}
+Used branches, stash, and pushed projects to GitHub.
+
+\section{Part D: LaTeX}
+This report was written using LaTeX and compiled with MiKTeX and VS Code.
+
+\end{document}
+
--- basic5.py
+++ basic5.py
@@ -3,7 +3,8 @@ fruits = ["apple", "banana", "cherry", "mango", "grape"]
 
 # Print first and last fruits
 print("First fruits:", fruits[0])
-print("Last fruit:", fruits[-1])
+print("Last fruit:", fruits[-1]cat .gitignore | grep tex
+)
 
 # Add one more fruit
 fruits.append("orange")
